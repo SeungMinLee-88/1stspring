9c6976267b0ff7ed926e7faf6f031fff68451913
lsm0428 <lsmlsm0428@gamil.com>
2025-03-25 17:33:38 +0900

    250325

--- src/main/java/com/spring/board/controller/RestBoardController.java
+++ src/main/java/com/spring/board/controller/RestBoardController.java
@@ -8,17 +8,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
-public class BoardController {
+@RequestMapping("/restboard")
+public class RestBoardController {
     private final BoardService boardService;
     private final CommentService commentService;
 
@@ -35,12 +34,12 @@ public class BoardController {
     }
 
     @GetMapping("/")
-    public String findAll(Model model) {
+    public List<BoardDTO> findAll(Model model) {
         // DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
         List<BoardDTO> boardDTOList = boardService.findAll();
         System.out.println("boardDTOList : " + boardDTOList.toString());
-        model.addAttribute("boardList", boardDTOList);
-        return "list";
+        //model.addAttribute("boardList", boardDTOList);
+        return boardDTOList;
     }
 
     @GetMapping("/{id}")
@@ -90,7 +89,7 @@ public class BoardController {
     }
 
     @GetMapping("/paging")
-    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model){
+    public Page<BoardDTO> paging(@PageableDefault(page = 1) Pageable pageable, Model model){
         //pageable.getPageNumber();
         Page<BoardDTO> boardList = boardService.paging(pageable);
         int blockLimit = 3;
@@ -100,7 +99,17 @@ public class BoardController {
         model.addAttribute("boardList", boardList);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
-        return "paging";
+        return boardList;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
